94e8ec5a6b89479bc58a415948be998501142e88
Xopin16 <rivas.fujos20@triana.salesianos.edu>
2023-03-19 07:39:01 +0100

    Agrego proyecto base

---
+++ tecnocholloapp/src/main/java/com/salesianostriana/dam/tecnocholloapp/TecnocholloappApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.tecnocholloapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecnocholloappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecnocholloappApplication.class, args);
+	}
+
+}
