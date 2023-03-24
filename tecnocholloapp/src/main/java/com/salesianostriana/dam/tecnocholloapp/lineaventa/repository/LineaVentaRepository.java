e48633a6efe65ba14218eb1463bcc2ff02771e57
Jose Ignacio Rivas Fuentes <rivas.fujos20@triana.salesianos.edu>
2023-03-24 13:33:04 +0100

    Agrego venta controller y memoria

--- tecnocholloapp/src/main/java/com/salesianostriana/dam/tecnocholloapp/lineaventa/repository/LineaVentaRepository.java
+++
@@ -1,4 +0,0 @@
-package com.salesianostriana.dam.tecnocholloapp.lineaventa.repository;
-
-public interface LineaVentaRepository {
-}
--- tecnocholloapp/src/main/java/com/salesianostriana/dam/tecnocholloapp/lineaventa/service/LineaVentaService.java
+++
@@ -1,9 +0,0 @@
-package com.salesianostriana.dam.tecnocholloapp.lineaventa.service;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class LineaVentaService {
-}
