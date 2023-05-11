76e68ea154a675ee69d12cea0b92d06724341cf0
Xopin16 <rivas.fujos20@triana.salesianos.edu>
2023-05-11 15:22:56 +0200

    Agrego componentes

--- angular_tecnocholloapp/src/app/app.module.ts
+++ angular_tecnocholloapp/src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { CategoryListComponent } from './components/category-list/category-list.component';
+import { VentaListComponent } from './components/venta-list/venta-list.component';
+import { CartComponent } from './components/cart/cart.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    ProductListComponent,
+    CategoryListComponent,
+    VentaListComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
